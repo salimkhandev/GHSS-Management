edd25db9582e5eef87f6887c227332e5afcead09
salimkhandev <salimt450s@gmail.com>
2024-10-10 16:05:29 +0500

    improved

--- backend/helo.js
+++ backend/helo.js
@@ -35,7 +35,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-// app.listen(3000, () => {
-//    console.log(`Server is running on http://localhost:3000`);
-// });
-// module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,37 @@
 const express = require('express');
-
+const pool = require('./dbConfig')
 const app = express();
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+
+
+const classSectionRoutes = require('./Routes/classSectionRoutes'); // Adjust the path as needed
+const studentRoutes = require('./Routes/registerStudent');
+const bulkupload = require('./Routes/bulkupload')
+const studentsAttendance = require('./Routes/studentsAttendance');
+
+
+
+// Use cookie-parser middleware
+app.use(cookieParser());
+app.use(cors({
+   origin: 'https://ghss-management.vercel.app', // Your frontend's origin
+   credentials: true // Allow cookies to be sent
+}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+
+app.use('/', classSectionRoutes);
+app.use('/', studentsAttendance);
+app.use('/students/bulk', bulkupload);
+app.use('/', studentRoutes); // Use student routes
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+   res.json("helo salim")
 });
- 
-let port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-   console.log(`Server is running on http://localhost:3000`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
