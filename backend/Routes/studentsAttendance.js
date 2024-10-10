2f6187dbf85b4150a60d36def75660365ddb2e8e
salimkhandev <salimt450s@gmail.com>
2024-10-10 20:43:00 +0500

    improved

--- backend/Routes/studentsAttendance.js
+++ backend/Routes/studentsAttendance.js
@@ -1,7 +1,7 @@
 const pool = require('../dbConfig');
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
