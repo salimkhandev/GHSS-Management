846effe7910680fa4457a916e889d3aa1b44496f
salimkhandev <salimt450s@gmail.com>
2025-01-30 12:21:08 +0500

    improved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/studentlist" element={<StudentList />} /> */}
+          <Route path="/studentlist" element={<StudentList />} />
           <Route path="/contact" element={<StudentForm />} />
           <Route path="/PerformanceDashboard" element={<PerformanceDashboard />} />
 
