bfd7c1f72fe34518fde444c12bfce4258cccf32c
salimkhandev <salimt450s@gmail.com>
2025-02-16 16:52:06 +0500

    improved

--- backend/Routes/handleAuth/logout.js
+++ backend/Routes/handleAuth/logout.js
@@ -3,18 +3,22 @@ const router = express.Router();
 
 
 router.post("/", (req, res) => {
-    console.log("Logout request received");
+    // error handling
+    if (!req.cookies) {
+        return res.status(400).json({ error: "No cookies found" });
+    }
+    console.log("Logo   ut request received");
     res.clearCookie('adminToken', {
         httpOnly: true,  // Set to false for simplicity
         sameSite: 'None',
         secure: true,
-        maxAge: 0 // 1 hour
+        maxAge: 3600000 // 1 hour
     });
     res.clearCookie('teacherToken', {
         httpOnly: true,  // Set to false for simplicity
         sameSite: 'None',
         secure: true,
-        maxAge: 0 // 1 hour
+        maxAge: 3600000 // 1 hour
     });
     
     res.status(200).json({ message: "Logged out successfully" });
