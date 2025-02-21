ccdd61aed646591cd82faa07fe0606908a480943
salimkhandev <salimt450s@gmail.com>
2025-02-21 15:15:32 +0500

    improved

--- backend/Routes/AttenPercentageForPieChart/dailyAttenPercentage.js
+++ backend/Routes/AttenPercentageForPieChart/dailyAttenPercentage.js
@@ -7,21 +7,41 @@ router.get('/', async (req, res) => {
     console.log('Fetching attendance data for class_id:', class_id,'section_id:', section_id);
      // Fetch class_id and section_id from query params
 
+// comment it 
+    // SELECT
+    // class_id,
+    //     section_id,
+    //     TO_CHAR(attendance_date, 'YYYY-MM-DD') AS attendance_date,
+    //         ROUND(SUM(CASE WHEN status = 'Present' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 0) AS present_percentage,
+    //             ROUND(SUM(CASE WHEN status = 'Absent' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 0) AS absent_percentage
+    // FROM
+    // attendance
+    // WHERE
+    // class_id = $1 AND section_id = $2
+    //         GROUP BY
+    // class_id, section_id, attendance_date
+    //             order by attendance_date DESC;
+
     try {
         const result = await pool.query(`
-            SELECT 
-                class_id,
-                section_id,
-                TO_CHAR(attendance_date, 'YYYY-MM-DD') AS attendance_date,
-                ROUND(SUM(CASE WHEN status = 'Present' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 0) AS present_percentage,
-                ROUND(SUM(CASE WHEN status = 'Absent' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 0) AS absent_percentage
-            FROM 
-                attendance
-            WHERE
-                class_id = $1 AND section_id = $2
-            GROUP BY 
-                class_id, section_id, attendance_date
-                order by attendance_date DESC;
+         SELECT 
+    class_id,
+    section_id,
+    TO_CHAR(attendance_date, 'YYYY-MM-DD') AS attendance_date,
+    ROUND(SUM(CASE WHEN status = 'Present' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 0) AS present_percentage,
+    ROUND(SUM(CASE WHEN status = 'Absent' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 0) AS absent_percentage
+FROM 
+    attendance
+WHERE
+    class_id = $1 
+    AND section_id = $2
+    AND attendance_date = (SELECT MAX(attendance_date) FROM attendance WHERE class_id = $1 AND section_id = $2)
+GROUP BY 
+    class_id, section_id, attendance_date
+ORDER BY 
+    attendance_date DESC
+LIMIT 1;
+
         `, [class_id, section_id]); // Using parameterized query to prevent SQL injection
 
         res.json(result.rows); // Send the filtered data to the frontend
